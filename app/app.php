bf98c4772f819d974da198e85ad8ff062ad28463
Jason Wuerch <wuerchjason@gmail.com>
2016-02-26 13:38:15 -0800

    create template for all stylists

--- app/app.php
+++ app/app.php
@@ -63,7 +63,7 @@
         return $app['twig']->render('client.html.twig', array('client' => $client, 'stylist_name' => $stylist_name));
     });
 
-    $app->get("/stylists", function($) use ($app) {
+    $app->get("/stylists", function() use ($app) {
         $stylists = Stylist::getAll();
         return $app['twig']->render('stylists.html.twig', array('stylists' => $stylists));
     });
