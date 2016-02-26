1f627f5e0fcfe1ea05b4e592896c6085f6cb2b20
Jason Wuerch <wuerchjason@gmail.com>
2016-02-26 11:26:05 -0800

    create header template for all twig pages

--- app/app.php
+++ app/app.php
@@ -1,5 +1,5 @@
 <?php
-    require_once __DIR__."/..vendor/autoload.php";
+    require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Client.php";
     require_once __DIR__."/../src/Stylist.php";
 
@@ -12,8 +12,9 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"));
 
-    use Symfony\Component\HttpFoundtaion\Request;
+    use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
+
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig');
